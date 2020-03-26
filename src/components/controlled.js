ccf0283621e8d8efdc46b0a4407a3acc9d9ec88d
AbrahamTerfie <abrahamterfie1236@gmail.com>
2020-03-26 23:17:30 +0300

    first commit

---
+++ src/components/controlled.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Controlled extends Component {
+
+    state = {
+        
+    }
+
+    render(){
+        return(
+            <div>
+                Controlled
+            </div>
+        )
+    }
+}
+
+export default Controlled;
---
+++ src/components/uncontrolled.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Uncontrolled extends Component {
+
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div>
+                Uncontrolled
+            </div>
+        )
+    }
+}
+
+export default Uncontrolled;
